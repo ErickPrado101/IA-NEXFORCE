91ec49445f3fe859f7cac32542f0139156d6acf6
Erick_Prado404 <73625446+ErickPrado101@users.noreply.github.com>
2024-01-26 00:34:48 -0300

    Delete sisitemas pedidos directory

--- sisitemas pedidos/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./bin/index');
-const keys = require('./bin/keys');
-const connection = require('./src/middleware/connection')
-
-app.listen(keys.server.port, ()=>{
-    connection.connect();
-    console.log('rodando a bagaceira')
-});
